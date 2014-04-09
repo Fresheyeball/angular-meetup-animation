4717998bd0897af812d3cafdba10f67adfbf4105
isaac.shapira <isaac.shapira@logrhythm.com>
2014-04-09 14:53:00 -0600

    moe moe moe

--- App/js/controllers/collection.ctrl.js
+++ App/js/controllers/collection.ctrl.js
@@ -1,16 +1,22 @@
 (function(){
   angular.module('meetup').controller('collectionCtrl', function($scope){
-    $scope.students = [{
-      name : "Tom"
-    },{
-      name : "Dick"
-    },{
-      name : "Sally"
-    }];
+    $scope.students = [
+      "Tom",
+      "Dick",
+      "Sally"
+    ];
 
     $scope.addNew = function(){
-      $scope.students.push({name : $scope.new});
+      $scope.students.push($scope.new);
       $scope.new = null;
     };
+
+    $scope.remove = function(student){
+      var index = $scope.students.indexOf(student);
+      $scope.students.splice(index, 1);
+    };
+
+    $scope.animClass = 'slide';
+
   });
 }).call(this);

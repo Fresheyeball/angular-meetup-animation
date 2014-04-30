4c236e2c0da2cfa870b9c57b52f17f38b487dd1f
Isaac Shapira <fresheyeball@gmail.com>
2014-04-30 08:15:56 -0600

    counter progress

--- App/js/controllers/counter.ctrl.js
+++ App/js/controllers/counter.ctrl.js
@@ -1,4 +1,6 @@
 (function(){
   angular.module('meetup').controller('counterCtrl', function($scope){
+    $scope.count = 0;
+    $scope.anim  = 'pop';
   });
 }).call(this);
---
+++ App/js/directives/counter.js
@@ -0,0 +1,19 @@
+(function(){
+  angular.module('meetup').directive('counter', function($animate){
+    return {
+      scope    : true,
+      template : "<div class='count' ng-repeat='char in chars'>{{char}}</div>",
+      link     : function (scope, element, attrs){
+        scope.$watch(attrs.count, function(val){
+          if(val)
+            { scope.chars = val.split(''); }
+
+          // $animate.addClass(scope.$eval(attrs.countAnimation));
+        });
+        scope.$watch(attrs.countAnimation, function(){
+
+        });
+      }
+    }
+  });
+}).call(this);

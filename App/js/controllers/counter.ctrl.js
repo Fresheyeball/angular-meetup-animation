22a56dba090f75e851a140134458b76c45915aed
Isaac Shapira <fresheyeball@gmail.com>
2014-05-03 22:16:48 -0600

    select

--- App/js/controllers/counter.ctrl.js
+++ App/js/controllers/counter.ctrl.js
@@ -1,9 +1,13 @@
 (function(){
   angular.module('meetup').controller('counterCtrl', function($scope, $interval){
-    $scope.count = 0;
-    $scope.anim  = 'pop';
+    
+    $scope.count       = 0;
+    $scope.animOptions = ['pop','spin'];
+    $scope.anim        = $scope.animOptions[0];
+
     $interval(function(){
       $scope.count++;
     }, 1000);
+    
   });
 }).call(this);

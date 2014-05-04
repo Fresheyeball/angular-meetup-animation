0016d47d04de2ba0d251065daefdf4f787a8373e
Isaac Shapira <fresheyeball@gmail.com>
2014-05-04 17:10:56 -0600

    moe moe moe

--- App/js/controllers/swap.ctrl.js
+++ App/js/controllers/swap.ctrl.js
@@ -1,5 +1,5 @@
 (function(){
-  var STATES = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyomin'];
+  var STATES = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 
   angular.module('meetup').controller('swapCtrl', function($scope){
     $scope.standingStates = [];
--- App/js/directives/swap.js
+++ App/js/directives/swap.js
@@ -1,22 +1,57 @@
 (function(){
-  var amm   = angular.module('meetup');
+  var amm   = angular.module('meetup'),
+      CANDY_HEIGHT = 35;
 
-  amm.directive('swappable', function(){
+  amm.directive('swappable', function($timeout){
     return {
-      controller : function(){
-        var i = 0;
-        this.test = function(){
-          console.log(i++);
+      scope : true,
+      controller : function($scope, $attrs){
+        this.elems = [];
+
+        $scope.$watch($attrs.swappable, function(val){ $scope.states = _.sortBy(val, 'name'); });
+        
+        this.addElement = function(element){
+          this.elems.push(element);
         };
+        this.removeElement = function(element){
+          console.log('remove', element);
+          this.elems = _.without(this.elems, element);
+        };
+
+      },
+      link : function(scope, element, attrs, ctrl){
+
+        $timeout(function(){
+          TweenLite.set(element[0], {
+            height : scope.states.length * CANDY_HEIGHT
+          });
+        }, 0, false);
+
+        scope.$watchCollection('states', function(){
+          var tweens = [];
+          $timeout(function(){
+            console.log('watcher', attrs.swappable);
+            for(var i = 0; i < ctrl.elems.length; i++){
+              tweens.push(new TweenLite.to(ctrl.elems[i], 0.5, {
+                y : i * CANDY_HEIGHT
+              }));
+              new TimelineLite({tweens:tweens});
+            }
+          }, 0, false);
+        });
+
       }
     };
   });
 
-  amm.directive('swap', function(){
+  amm.directive('swap', function($timeout){
     return {
       require : '^swappable',
       link    : function(scope, element, attrs, ctrl){
-        ctrl.test();
+        ctrl.addElement(element);
+        scope.$on('$destroy', function(){
+          ctrl.removeElement(element);
+        });
       }
     };
   });

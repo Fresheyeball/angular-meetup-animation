c439f1fe6e7731814938d3e20c07d803eb83e9d5
Isaac Shapira <fresheyeball@gmail.com>
2014-05-04 16:45:53 -0600

    starting over on swap

---
+++ App/js/directives/swap.js
@@ -0,0 +1,12 @@
+(function(){
+  var amm   = angular.module('meetup');
+
+  amm.directive('swappable', function(){
+    return {};
+  });
+
+  amm.directive('swap', function(){
+    return {};
+  });
+
+}).call(this);

22dcba087caecedeb0244a7083c1b26e953b3300
Isaac Shapira <fresheyeball@gmail.com>
2014-05-04 16:48:40 -0600

    moe

--- App/js/directives/swap.js
+++ App/js/directives/swap.js
@@ -2,11 +2,23 @@
   var amm   = angular.module('meetup');
 
   amm.directive('swappable', function(){
-    return {};
+    return {
+      controller : function(){
+        var i = 0;
+        this.test = function(){
+          console.log(i++);
+        };
+      }
+    };
   });
 
   amm.directive('swap', function(){
-    return {};
+    return {
+      require : '^swappable',
+      link    : function(scope, element, attrs, ctrl){
+        ctrl.test();
+      }
+    };
   });
 
 }).call(this);
